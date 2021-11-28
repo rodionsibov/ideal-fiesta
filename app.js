a93f145538e094623d1df2434d0e50f473f885c7
Rodion Sibov <rodionsibov@gmail.com>
2021-11-28 22:16:52 +0200

    create save button

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let count = 0
-const countEl =  document.querySelector('#count-el')
-const btnEl = document.querySelector('#increment-btn')
+const countEl = document.querySelector('#count-el')
+const btnIncrement = document.querySelector('#increment-btn')
+const btnSave = document.querySelector('#save-btn')
 
-btnEl.addEventListener('click', increment)
+btnIncrement.addEventListener('click', increment)
+btnSave.addEventListener('click', save)
 
 function increment() {
     count++
-   countEl.innerHTML = count
+    countEl.innerHTML = count
+}
+
+function save() {
+    console.log(count);
 }
