188b7058766ed97f02716d2b75ae1a31291e6c8c
Rodion Sibov <rodionsibov@gmail.com>
2021-11-30 10:03:26 +0200

    update

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
-console.log(sum);
+if (sum < 21) {
+    console.log('Do you want to draw a new card? 😀');
+} else if (sum === 21) {
+    console.log('Wohoo! You have got a Blackjack! 🥳');
+    hasBlackJack = true
+} else if (sum > 21) {
+    console.log('You are out of the game! 😔');
+}
