a619eb1c445a9ed7b59f1fb7e4ece37e1e8a708c
Rodion Sibov <rodionsibov@gmail.com>
2021-11-30 11:27:44 +0200

    update

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ''
+const btnStartGameEl = document.querySelector('#start-game-el')
+const btnNewCardEl = document.querySelector('#new-card-el')
+const messageEl = document.querySelector('#message-el')
+const sumEl = document.querySelector('#sum-el')
+const cardsEl = document.querySelector('#cards-el')
 
-if (sum < 21) {
-    message = 'Do you want to draw a new card? 😀'
-} else if (sum === 21) {
-    message = 'Wohoo! You have got a Blackjack! 🥳'
-    hasBlackJack = true
-} else {
-    message = 'You are out of the game! 😔'
-    isAlive = false
+function startGame() {
+    cardsEl.innerText = `Cards: ${firstCard} ${secondCard}`
+    sumEl.innerText = `Sum: ${sum}`
+    if (sum < 21) {
+        message = 'Do you want to draw a new card?'
+    } else if (sum === 21) {
+        message = 'You have got a Blackjack!'
+        hasBlackJack = true
+    } else {
+        message = 'You are out of the game!'
+        isAlive = false
+    }
+    messageEl.innerText = message
 }
+
+function newCard() {
+    
+}
+
+btnStartGameEl.addEventListener('click', startGame)
+btnNewCardEl.addEventListener('click', newCard)
