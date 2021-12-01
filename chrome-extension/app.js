58f706820a2fbbeafd2af9291feb5bf39b770d42
Rodion Sibov <rodionsibov@gmail.com>
2021-12-01 15:35:31 +0200

    update

---
+++ chrome-extension/app.js
@@ -0,0 +1,9 @@
+const inputBtn = document.querySelector('#input-btn')
+const myLeads = []
+const inputEl = document.querySelector('#input-el')
+
+function saveLead() {
+
+}
+
+inputBtn.addEventListener('click', saveLead) 
