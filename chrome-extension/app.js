e358b4a25f1b0ac58ad564be2dac676c5ead411a
Rodion Sibov <rodionsibov@gmail.com>
2021-12-02 09:43:52 +0200

    update

--- chrome-extension/app.js
+++ chrome-extension/app.js
@@ -1,10 +1,16 @@
 const inputBtn = document.querySelector('#input-btn')
 const inputEl = document.querySelector('#input-el')
 const ulEl = document.querySelector('#ul-el')
-const myLeads = []
+let myLeads = []
+
+if (localStorage.getItem('myLeads')) {
+    myLeads = JSON.parse(localStorage.getItem('myLeads'))
+    renderLeads()
+}
 
 function saveLead() {
     if (inputEl.value) myLeads.push(inputEl.value)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
     inputEl.value = ''
     renderLeads()
 }
